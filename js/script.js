22537d50af927a7f4c97faeebeac1fead4ff8d83
kiet <kiet123pascal@gmail.com>
2023-11-09 00:24:04 +0700

    added about page

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('menu-button')[0];
-const navLinks = document.getElementById('nav-links');
-toggleButton.addEventListener('click', function () {
-    for (var i = 0; i < navLinks.length; i++)
-        navLinks[i].classList.toggle('active');
-})
